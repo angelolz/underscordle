1d3b000a9b5763e921e962a2c1d4b5c8525bf436
angelolz <angel.lugia98@gmail.com>
2026-05-04 20:49:44 -0600

    added settings (wip)

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,8 +1,14 @@
 <script lang="ts">
     import logo from '$lib/assets/underscordle.svg';
+    import { resolve } from '$app/paths';
+    import { ArchiveOutline, CogOutline, QuestionCircleOutline } from 'flowbite-svelte-icons';
+    import Modal from './Modal.svelte';
+
+    let showHelpModal = $state(true);
+    let showSettingsModal = $state(false)
 </script>
 
-<div class="flex w-full flex-col items-center justify-center gap-4 pt-4 align-middle">
+<div class="flex w-full flex-col items-center justify-center gap-2 pt-4 align-middle">
     <img src={logo} alt="underscordle" class="h-8 w-auto sm:h-10" />
     <span class="px-4 text-center">
         <p class="text-sm text-white">A daily underscores song guessing game!</p>
@@ -10,5 +16,22 @@
             by <a class="hover:underline" target="_blank" href="https://angelolz.one">angelolz</a>
         </p>
     </span>
-    <hr class="h-5 w-full max-w-[280px] border-gray-500 py-2 sm:max-w-sm" />
+    <div class="flex flex-row gap-2 items-center text-white">
+        <span class="hover:scale-105 active:scale-95 transition-all flex items-center"><a href={resolve('/archive')}><ArchiveOutline class="shrink-0 h-5 w-5"/></a></span>
+        <span class="hover:scale-105 active:scale-95 transition-all flex items-center"><button onclick={() => {showHelpModal = true}}><QuestionCircleOutline class="shrink-0 h-5 w-5"/></button></span>
+        <span class="hover:scale-105 active:scale-95 transition-all flex items-center"><button onclick={() => {showSettingsModal = true}}><CogOutline class="shrink-0 h-5 w-5"/></button></span>
+    </div>
+    <hr class="h-3 w-full max-w-[280px] border-gray-500 sm:max-w-sm" />
 </div>
+
+<Modal revealed={showHelpModal} onClose={() => {showHelpModal = false}}>
+    <div class="flex flex-col items-center gap-4">
+        <span class="font-bold text-white text-xl">How to Play:</span>
+        <ul class="text-sm text-left text-white list-outside list-disc space-y-2 pl-4">
+            <li>Each day you will have 5 songs to guess, with 3 guesses each.</li>
+            <li>Click the play button to hear the clue and type in the box to make your guess.</li>
+            <li>Feel free to come back later, progress is always saved.</li>
+        </ul>
+    </div>
+</Modal>
+<Modal revealed={showSettingsModal} onClose={() => {showSettingsModal = false}}>Test Settings</Modal>
---
+++ src/lib/components/Modal.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+    import { fly } from "svelte/transition";
+
+
+    const {children, revealed, onClose} = $props();
+</script>
+
+{#if revealed}
+    <div transition:fly={{ y: 10, duration: 250 }} class="fixed inset-0 flex items-center justify-center bg-black/65 z-50 animate-fade-in animate">
+        <div class="bg-[#121212] rounded-lg border-2 border-white w-full max-w-md mx-3 sm:mx-0 p-6 relative animate-fly-fade-in text-center">
+            <button onclick={onClose} class="absolute top-2 right-2 text-gray-400 hover:text-white transition-colors" title="close">
+                <svg
+                    class="w-6 h-6"
+                    fill="none"
+                    stroke="currentColor"
+                    viewBox="0 0 24 24"
+                    xmlns="http://www.w3.org/2000/svg"
+                >
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width={2} d="M6 18L18 6M6 6l12 12" />
+                </svg>
+            </button>
+            {@render children()}
+        </div>
+    </div>
+{/if}
